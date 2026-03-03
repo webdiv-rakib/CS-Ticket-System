279bbbec9b5b7028fa330141ae14529c8706a0e2
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-03 20:25:04 +0600

    NabBar Design implemented, Button BG added, Hover to change Menu item color change.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <NavBar></NavBar>
     </>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className="navbar shadow-sm inter md:flex justify-between px-4 md:px-10">
+            <div>
+                <a className="text-2xl font-bold cursor-pointer">CS Ticket System</a>
+            </div>
+            <div className="hidden md:flex md:items-center md:gap-5">
+                <a href="" className="hover:text-gray-600">Home</a>
+                <a href="" className="hover:text-gray-600">FAQ</a>
+                <a href="" className="hover:text-gray-600">Changelog</a>
+                <a href="" className="hover:text-gray-600">Blog</a>
+                <a href="" className="hover:text-gray-600">Download</a>
+                <a href="" className="hover:text-gray-600">Contact</a>
+                <button className="btn border-none text-white px-6 hover:opacity-90" style={{ background: 'linear-gradient(90deg, #422AD5 0%, #6A54F0 100%)' }}>+ New Ticket</button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
