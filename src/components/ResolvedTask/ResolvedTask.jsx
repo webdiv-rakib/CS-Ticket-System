1c4b07cff9365c77530b4f67afc816d75400cb56
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-05 03:59:49 +0600

    some ui update.

--- src/components/ResolvedTask/ResolvedTask.jsx
+++ src/components/ResolvedTask/ResolvedTask.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ResolvedTask = ({ ticket }) => {
     return (
-        <div className='card p-2 bg-base-300 space-y-2 mt-5'>
-            <h1 className='text-xl font-semibold'>{ticket.title}</h1>
+        <div className='card p-2 bg-[#e0e7ff] space-y-2 mt-5'>
+            <h1 className='text-xl font-semibold md:py-5 md:px-5'>{ticket.title}</h1>
         </div >
     );
 };
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Task = ({ ticket, handleComplete }) => {
 
     return (
-        <div className='card p-2 bg-base-300 space-y-2 mt-5'>
+        <div className='card md:p-5 bg-base-300 space-y-2 mt-5'>
             <h1 className='text-xl font-semibold'>{ticket.title}</h1>
             <button onClick={() => handleComplete(ticket)} className='btn bg-green-600 text-white'>Completed</button>
         </div >
